27ced0be927e1b8d3000c69e9d281ddc0c61c0b3
unknown <kcross8@gmail.com>
2020-06-05 13:25:02 -0400

    first commit

---
+++ ApiGroupProject/Models/Favorites.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIGroupProject.Models
+{
+    public class Favorites
+    {
+        [Key]
+        public string title { get; set; }
+        public string href { get; set; }
+        public string ingredients { get; set; }
+        public string thumbnail { get; set; }
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual AspNetUsers User { get; set; }
+    }
+}
---
+++ ApiGroupProject/Models/Recipe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIGroupProject.Models
+{
+    using System;
+    namespace ApiGroupProject
+    {
+        public class Recipe
+        {
+            public string title { get; set; }
+            public string href { get; set; }
+            public string ingredients { get; set; }
+            public string thumbnail { get; set; }
+        }
+    }
+
+}
